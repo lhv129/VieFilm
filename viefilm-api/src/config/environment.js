68bb25cd7f27fd434aead4210f2584a85aada2bd
DESKTOP-MQR0I53\vietlh <lhv2k4@gmail.com>
2025-04-18 22:27:45 +0700

    config cloudinary

--- viefilm-api/src/config/environment.js
+++ viefilm-api/src/config/environment.js
@@ -8,7 +8,7 @@ export const env = {
     APP: process.env.APP,
 
 
-    // CLOUD_NAME: process.env.CLOUD_NAME,
-    // API_KEY: process.env.API_KEY,
-    // API_SECRET: process.env.API_SECRET,
+    CLOUD_NAME: process.env.CLOUD_NAME,
+    API_KEY: process.env.API_KEY,
+    API_SECRET: process.env.API_SECRET,
 }
