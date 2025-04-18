6f263441c243a2dd941d80cc3d57a3281a8f69a4
DESKTOP-MQR0I53\vietlh <lhv2k4@gmail.com>
2025-04-18 22:25:48 +0700

    config MongoDB

---
+++ viefilm-api/src/config/environment.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+
+export const env = {
+    MONGODB_URI: process.env.MONGODB_URI,
+    DATABASE_NAME: process.env.DATABASE_NAME,
+    APP_HOST: process.env.APP_HOST,
+    APP_PORT: process.env.APP_PORT,
+    APP: process.env.APP,
+
+
+    // CLOUD_NAME: process.env.CLOUD_NAME,
+    // API_KEY: process.env.API_KEY,
+    // API_SECRET: process.env.API_SECRET,
+}
